1de75446f1aa72f8633849e08d5f2c9a43256bc1
maggie-moheb <maggiemoheb@gmail.com>
2015-12-15 10:33:53 +0200

    adding followers list api

--- Rails/app/controllers/followers_controller.rb
+++ Rails/app/controllers/followers_controller.rb
@@ -1,3 +1,7 @@
 class FollowersController < ApplicationController
-    
+    def findFollowers
+    	@user = User.find(params[:user_id])
+    	@followers = @user.followers.all
+    	render json: @followers if stale?(@followers)
+    end
 end
